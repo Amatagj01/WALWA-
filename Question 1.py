7a4c18b4721441f9973bc0ad0e94fb62515365d8
Walwa16 <walwakusekwa@gmail.com>
2025-07-02 17:49:24 +0300

    Update Question 1.py

--- Question 1.py
+++ Question 1.py
@@ -7,8 +7,7 @@ Write a Python program to:
 1. Calculate the average yield.
 2. Find the year with the highest and lowest yield (by value).
 3. Determine the number of years the yield was above 5.0 tonnes."""
-
-     #Crop Yield Data Analysis 
+ #Crop Yield Data Analysis 
      
 #Maize yield in tonnes from the university farm for the past ten years
 
@@ -25,9 +24,9 @@ total = sum(yield_data)
 average = total /len(yield_data)
 print("The average yield of Maize for the past ten years is:",average)
 print("______________________________________________________")
-# Years with highest and lowest Maize yield 
 
-#The year with lowest Maize yield.
+# Step 2:Years with highest and lowest Maize yield
+# 2.i: The year with lowest Maize yield.
 for data in yield_data:
   lowest_data = yield_data[0]
   if data < lowest_data:
@@ -37,8 +36,9 @@ lowest_data_index +=1
 print("The year with lowest Maize yield data is year",lowest_data_index)
 
 print("_______________________________________________________")
-for data in yield_data: 
 
+#Step2. ii: The Year with highest Maize yield data 
+for data in yield_data: 
   highest_data = yield_data[0]
   if data > highest_data:
     highest_data = data
